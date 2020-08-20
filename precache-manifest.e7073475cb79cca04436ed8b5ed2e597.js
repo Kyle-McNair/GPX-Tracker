f4905df43b7eab03b27a861438dab7e62fc2f388
Kyle McNair <kwmcnair@shc.wisc.edu>
2020-08-20 14:19:16 -0500

    Updates

---
+++ precache-manifest.e7073475cb79cca04436ed8b5ed2e597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58dcc860947e5f22391b1ec558ca8ab",
+    "url": "/GPX-Tracker/index.html"
+  },
+  {
+    "revision": "08d5d1658af4edc80927",
+    "url": "/GPX-Tracker/static/css/main.68365989.chunk.css"
+  },
+  {
+    "revision": "ad542d65d6594f458b85",
+    "url": "/GPX-Tracker/static/js/2.5fc3941d.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/GPX-Tracker/static/js/2.5fc3941d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d5d1658af4edc80927",
+    "url": "/GPX-Tracker/static/js/main.49a03b4a.chunk.js"
+  },
+  {
+    "revision": "7191be7f7f5c26f76396",
+    "url": "/GPX-Tracker/static/js/runtime-main.a887ed9b.js"
+  },
+  {
+    "revision": "f63a5087da114e7538d90559a78915b4",
+    "url": "/GPX-Tracker/static/media/activity.f63a5087.gpx"
+  }
+]);
